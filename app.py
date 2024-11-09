6ff1c7e63ec08fb6a9256ab65096a371a38cad28
huyjaky <huyphanuyg@gmail.com>
2024-11-09 20:11:54 +0700

    first commit

--- app.py
+++ app.py
@@ -72,36 +72,6 @@ class VideoProcessor:
         return av.VideoFrame.from_ndarray(img, format="bgr24")
     
 
-def read_license_plate(license_plate_crop, img):
-    scores = 0
-    detections = reader.readtext(license_plate_crop)
-
-    width = img.shape[1]
-    height = img.shape[0]
-    
-    if detections == [] :
-        return None, None
-
-    rectangle_size = license_plate_crop.shape[0]*license_plate_crop.shape[1]
-
-    plate = [] 
-
-    for result in detections:
-        length = np.sum(np.subtract(result[0][1], result[0][0]))
-        height = np.sum(np.subtract(result[0][2], result[0][1]))
-        
-        if length*height / rectangle_size > 0.17:
-            bbox, text, score = result
-            text = result[1]
-            text = text.upper()
-            scores += score
-            plate.append(text)
-    
-    if len(plate) != 0 : 
-        return " ".join(plate), scores/len(plate)
-    else :
-        return " ".join(plate), 0
-
 
 # NOTE: detect id vehicles
 def model_prediction(img):
@@ -138,32 +108,17 @@ def model_prediction(img):
 
             license_plate_crop = img[int(y1):int(y2), int(x1): int(x2), :]
 
-            img_name = '{}.jpg'.format(uuid.uuid1())
-         
-            cv2.imwrite(os.path.join(folder_path, img_name), license_plate_crop)
+            # Convert and save the cropped license plate image
+            license_plate_crop_rgb = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2RGB)
 
-            license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY) 
-
-            license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_gray, img)
-
-            licenses_texts.append(license_plate_text)
-
-            if license_plate_text is not None and license_plate_text_score is not None  :
-                license_plate_crops_total.append(license_plate_crop)
-                results[license_numbers] = {}
-                
-                results[license_numbers][license_numbers] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2], 'car_score': car_score},
-                                                        'license_plate': {'bbox': [x1, y1, x2, y2],
-                                                                            'text': license_plate_text,
-                                                                            'bbox_score': score,
-                                                                            'text_score': license_plate_text_score}} 
-                license_numbers+=1
-          
-        write_csv(results, f"./csv_detections/detection_results.csv")
-
-        img_wth_box = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
-        return [img_wth_box, licenses_texts, license_plate_crops_total]
+            # Save the image to a specific path
+            output_path = "license_plate_crop.jpg"  # Update the path as needed
+            cv2.imwrite(output_path, cv2.cvtColor(license_plate_crop_rgb, cv2.COLOR_RGB2BGR))
+            
+            res = model.chat(tokenizer, './license_plate_crop.jpg', ocr_type='ocr')
+            print(res)
+            
+            return {'License_plate_crop': license_plate_crop_rgb, 'License_id': res}
     
     else : 
         img_wth_box = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -227,12 +182,12 @@ with body :
     
     if img is not None:
         image = np.array(Image.open(img))    
-        col2.image(image, width=400)
+        col2.image(image)
 
         if col5.button("Apply Detection"):
             results = model_prediction(image)
 
-            if len(results) == 3 :
+            if results['License_plate_crop']:
                 prediction, texts, license_plate_crop = results[0], results[1], results[2]
 
                 texts = [i for i in texts if i is not None]
@@ -274,8 +229,6 @@ with body :
                         col10.image(license_plate_crop[i], width=350)
                         col11.success(f"License Number {i}: {texts[i]}")
 
-                    df = pd.read_csv(f"./csv_detections/detection_results.csv")
-                    st.dataframe(df)
             else :
                 prediction = results[0]
                 _, col3, _ = st.columns([0.4,1,0.2])
