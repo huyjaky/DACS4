54e10f79de76ec31573ed79b191b76d430ebc83d
huyjaky <huyphanuyg@gmail.com>
2025-05-05 21:23:16 +0700

    DACN1

--- app.py
+++ app.py
@@ -4,188 +4,174 @@ from PIL import Image
 from ultralytics import YOLO
 import cv2
 import easyocr
-import pandas as pd
-from util import set_background, write_csv
-import uuid
-import os
-from  streamlit_webrtc import webrtc_streamer
-import av
+from util import set_background
 from transformers import AutoModel, AutoTokenizer
+from paddleocr import PaddleOCR
 
 set_background("./imgs/background.png")
 
+ocr = PaddleOCR(
+    lang="en"
+)  # The model file will be downloaded automatically when executed for the first time
 
-tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True)
-model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cpu', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
-model = model.eval().cuda()
+tokenizer = AutoTokenizer.from_pretrained("ucaslcl/GOT-OCR2_0", trust_remote_code=True)
+ocr_model = AutoModel.from_pretrained(
+    "ucaslcl/GOT-OCR2_0",
+    trust_remote_code=True,
+    low_cpu_mem_usage=True,
+    device_map="cuda",
+    use_safetensors=True,
+    pad_token_id=tokenizer.eos_token_id,
+)
+ocr_model = ocr_model.eval().cuda()
 
-folder_path = "./licenses_plates_imgs_detected/"
-LICENSE_MODEL_DETECTION_DIR = './models/results/runs/detect/train3/weights/best.onnx'
-COCO_MODEL_DIR = "./models/results/yolo11n.pt"
-
-reader = easyocr.Reader(['en'], gpu=False)
-
+detect_car_model = YOLO(
+    "/home/duckq1u/Documents/DoAnChuyenNganh/DACN1ver1/models/yolov8n.pt"
+)
+license_plate_model = YOLO(
+    "/home/duckq1u/Documents/DoAnChuyenNganh/DACN1ver1/models/model_ver2/best.pt"
+)
+reader = easyocr.Reader(["en"], gpu=True)
 vehicles = [2]
+
 
 header = st.container()
 body = st.container()
 
-# NOTE: run model!
-coco_model = YOLO(COCO_MODEL_DIR)
-license_plate_detector = YOLO(LICENSE_MODEL_DETECTION_DIR)
 
 threshold = 0.15
 
 state = "Uploader"
 
-if "state" not in st.session_state :
+if "state" not in st.session_state:
     st.session_state["state"] = "Uploader"
-
-class VideoProcessor:
-    def recv(self, frame) :
-        img = frame.to_ndarray(format="bgr24")
-        img_to_an = img.copy()
-        img_to_an = cv2.cvtColor(img_to_an, cv2.COLOR_RGB2BGR)
-        license_detections = license_plate_detector(img_to_an)[0]
-
-        if len(license_detections.boxes.cls.tolist()) != 0 :
-            for license_plate in license_detections.boxes.data.tolist() :
-                x1, y1, x2, y2, score, class_id = license_plate
-
-                cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 3)
-
-                license_plate_crop = img[int(y1):int(y2), int(x1): int(x2), :]
-            
-                license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY) 
-
-                license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_gray, img)
-
-                cv2.rectangle(img, (int(x1) - 40, int(y1) - 40), (int(x2) + 40, int(y1)), (255, 255, 255), cv2.FILLED)
-                cv2.putText(img,
-                            str(license_plate_text),
-                            (int((int(x1) + int(x2)) / 2) - 70, int(y1) - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            1,
-                            (0, 0, 0),
-                            3)
-
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
-    
 
 
 # NOTE: detect id vehicles
 def model_prediction(img):
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    license_plate_list = []
+    car_list = []
+    license_number_list_paddle = []
+    license_number_list_got = []
+    car_detect = detect_car_model(img)[0]
 
-    object_detections = coco_model(img)[0]
-    
-    license_detections = license_plate_detector(img)[0]
-
-    if len(object_detections.boxes.cls.tolist()) != 0 :
-
-        # NOTE: set variable model detect
-        for detection in object_detections.boxes.data.tolist() :
-            xcar1, ycar1, xcar2, ycar2, car_score, class_id = detection
-
-            if int(class_id) in vehicles :
-                cv2.rectangle(img, (int(xcar1), int(ycar1)), (int(xcar2), int(ycar2)), (0, 0, 255), 3)
-    else :
-            xcar1, ycar1, xcar2, ycar2 = 0, 0, 0, 0
-            car_score = 0
-
-
-
-    if len(license_detections.boxes.cls.tolist()) != 0 :
-        license_plate_crops_total = []
-
-        for license_plate in license_detections.boxes.data.tolist() :
-            x1, y1, x2, y2, score, class_id = license_plate
+    if car_detect.boxes.cls.tolist() != 0:
+        for car in car_detect.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = car
 
             cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 3)
 
-            license_plate_crop = img[int(y1):int(y2), int(x1): int(x2), :]
+            car_crop = img[int(y1) : int(y2), int(x1) : int(x2), :]
 
-            # Convert and save the cropped license plate image
-            license_plate_crop_rgb = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2RGB)
+            car_list.append(car_crop)
 
-            # Save the image to a specific path
-            output_path = "license_plate_crop.jpg"  # Update the path as needed
-            cv2.imwrite(output_path, cv2.cvtColor(license_plate_crop_rgb, cv2.COLOR_RGB2BGR))
-            
-            res = model.chat(tokenizer, './license_plate_crop.jpg', ocr_type='ocr')
-            print(res)
-            
-            return {'License_plate_crop': license_plate_crop_rgb, 'License_id': res}
-    
-    else : 
-        img_wth_box = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        return {'License_plate_crop': img_wth_box, 'License_id': None}
-    
+            license_plates = license_plate_model(car_crop)[0]
+            if license_plates.boxes.cls.tolist() != 0:
+                for license_plate in license_plates.boxes.data.tolist():
+                    x1, y1, x2, y2, score, class_id = license_plate
 
-def change_state_uploader() :
+                    cv2.rectangle(
+                        car_crop, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 3
+                    )
+
+                    license_plate_crop = car_crop[
+                        int(y1) : int(y2), int(x1) : int(x2), :
+                    ]
+
+                    # Convert and save the cropped license plate image
+                    license_plate_crop_rgb = cv2.cvtColor(
+                        license_plate_crop, cv2.COLOR_BGR2RGB
+                    )
+
+                    # Save the image to a specific path
+                    output_path = "license_plate_crop.jpg"  # Update the path as needed
+                    cv2.imwrite(
+                        output_path,
+                        cv2.cvtColor(license_plate_crop_rgb, cv2.COLOR_RGB2BGR),
+                    )
+                    license_plate_list.append(license_plate_crop)
+
+                    result = ocr.ocr(license_plate_crop)
+                    text = ""
+                    if result[0] is not None:
+                        for line in result[0]:
+                            text += line[1][0]
+                    license_number_list_paddle.append(text)
+                    res = ocr_model.chat(
+                        tokenizer, "./license_plate_crop.jpg", ocr_type="ocr"
+                    )
+                    license_number_list_got.append(res)
+
+    return {
+        "car_crop": car_list,
+        "license_crop": license_plate_list,
+        "license_number": {
+            "paddleocr": license_number_list_paddle,
+            "gotocr": license_number_list_got,
+        },
+    }
+
+
+def change_state_uploader():
     st.session_state["state"] = "Uploader"
 
-    
-def change_state_camera() :
+
+def change_state_camera():
     st.session_state["state"] = "Camera"
 
-def change_state_live() :
+
+def change_state_live():
     st.session_state["state"] = "Live"
-    
-with header :
-    _, col1, _ = st.columns([0.2,1,0.1])
+
+
+with header:
+    _, col1, _ = st.columns([0.2, 1, 0.1])
     col1.title("💥 License Car Plate Detection 🚗")
 
-    _, col0, _ = st.columns([0.15,1,0.1])
+    _, col0, _ = st.columns([0.15, 1, 0.1])
     col0.image("./imgs/test_background.jpg", width=500)
 
-
-    _, col4, _ = st.columns([0.1,1,0.2])
+    _, col4, _ = st.columns([0.1, 1, 0.2])
     col4.subheader("Computer Vision Detection with YoloV8 🧪")
 
-    _, col, _ = st.columns([0.3,1,0.1])
+    _, col, _ = st.columns([0.3, 1, 0.1])
     col.image("./imgs/plate_test.jpg")
 
-    _, col5, _ = st.columns([0.05,1,0.1])
+    _, col5, _ = st.columns([0.05, 1, 0.1])
 
-    st.write("The differents models detect the car and the license plate in a given image, then extracts the info about the license using EasyOCR, and crop and save the license plate as a Image, with a CSV file with all the data.   ")
+    st.write(
+        "The differents models detect the car and the license plate in a given image, then extracts the info about the license using EasyOCR, and crop and save the license plate as a Image, with a CSV file with all the data.   "
+    )
 
 
-with body :
-    _, col1, _ = st.columns([0.1,1,0.2])
+with body:
+    _, col1, _ = st.columns([0.1, 1, 0.2])
     col1.subheader("Check It-out the License Car Plate Detection Model 🔎!")
 
     _, colb1, colb2, colb3 = st.columns([0.2, 0.7, 0.6, 1])
 
-    if colb1.button("Upload an Image", on_click=change_state_uploader) :
-        pass
-    elif colb2.button("Take a Photo", on_click=change_state_camera) :
-        pass
-    elif colb3.button("Live Detection", on_click=change_state_live) :
-        pass
-
-    if st.session_state["state"] == "Uploader" :
+    if st.session_state["state"] == "Uploader":
         img = st.file_uploader("Upload a Car Image: ", type=["png", "jpg", "jpeg"])
-    elif st.session_state["state"] == "Camera" :
+    elif st.session_state["state"] == "Camera":
         img = st.camera_input("Take a Photo: ")
-    elif st.session_state["state"] == "Live" :
-        webrtc_streamer(key="sample", video_processor_factory=VideoProcessor)
+    elif st.session_state["state"] == "Live":
+        # webrtc_streamer(key="sample", video_processor_factory=VideoProcessor)
         img = None
 
-    _, col2, _ = st.columns([0.3,1,0.2])
+    _, col2, _ = st.columns([0.3, 1, 0.2])
 
-    _, col5, _ = st.columns([0.8,1,0.2])
+    _, col5, _ = st.columns([0.8, 1, 0.2])
 
-    
     if img is not None:
-        image = np.array(Image.open(img))    
+        image = np.array(Image.open(img))
         col2.image(image)
+        results = model_prediction(image)
 
-        if col5.button("Apply Detection"):
-            results = model_prediction(image)
-                
+        image_zone, model_prediction_zone = st.columns(2)
 
-
-
-
- 
+        with image_zone:
+            st.image(results["car_crop"])
+        with model_prediction_zone:
+            st.image(results["license_crop"])
+            st.title(results["license_number"]["paddleocr"])
+            st.title(results["license_number"]["gotocr"])
--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import easyocr
 import base64
 import streamlit as st
 
+
 def set_background(image_file):
     """
     This function sets the background of a Streamlit app to an image specified by the given image file.
@@ -25,23 +26,14 @@ def set_background(image_file):
     """
     st.markdown(style, unsafe_allow_html=True)
 
+
 # Initialize the OCR reader
-reader = easyocr.Reader(['en'], gpu=False)
+reader = easyocr.Reader(["en"], gpu=False)
 
 # Mapping dictionaries for character conversion
-dict_char_to_int = {'O': '0',
-                    'I': '1',
-                    'J': '3',
-                    'A': '4',
-                    'G': '6',
-                    'S': '5'}
+dict_char_to_int = {"O": "0", "I": "1", "J": "3", "A": "4", "G": "6", "S": "5"}
 
-dict_int_to_char = {'0': 'O',
-                    '1': 'I',
-                    '3': 'J',
-                    '4': 'A',
-                    '6': 'G',
-                    '5': 'S'}
+dict_int_to_char = {"0": "O", "1": "I", "3": "J", "4": "A", "6": "G", "5": "S"}
 
 
 def write_csv(results, output_path):
@@ -52,34 +44,50 @@ def write_csv(results, output_path):
         results (dict): Dictionary containing the results.
         output_path (str): Path to the output CSV file.
     """
-    with open(output_path, 'w') as f:
-        f.write('{},{},{},{},{},{},{}\n'.format('frame_nmr', 'car_id', 'car_bbox',
-                                                'license_plate_bbox', 'license_plate_bbox_score', 'license_number',
-                                                'license_number_score'))
+    with open(output_path, "w") as f:
+        f.write(
+            "{},{},{},{},{},{},{}\n".format(
+                "frame_nmr",
+                "car_id",
+                "car_bbox",
+                "license_plate_bbox",
+                "license_plate_bbox_score",
+                "license_number",
+                "license_number_score",
+            )
+        )
 
         for frame_nmr in results.keys():
             for car_id in results[frame_nmr].keys():
                 print(results[frame_nmr][car_id])
-                if 'car' in results[frame_nmr][car_id].keys() and \
-                   'license_plate' in results[frame_nmr][car_id].keys() and \
-                   'text' in results[frame_nmr][car_id]['license_plate'].keys():
-                    f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr,
-                                                            car_id,
-                                                            '[{} {} {} {}]'.format(
-                                                                results[frame_nmr][car_id]['car']['bbox'][0],
-                                                                results[frame_nmr][car_id]['car']['bbox'][1],
-                                                                results[frame_nmr][car_id]['car']['bbox'][2],
-                                                                results[frame_nmr][car_id]['car']['bbox'][3]),
-                                                            '[{} {} {} {}]'.format(
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][0],
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][1],
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][2],
-                                                                results[frame_nmr][car_id]['license_plate']['bbox'][3]),
-                                                            results[frame_nmr][car_id]['license_plate']['bbox_score'],
-                                                            results[frame_nmr][car_id]['license_plate']['text'],
-                                                            results[frame_nmr][car_id]['license_plate']['text_score'])
-                            )
+                if (
+                    "car" in results[frame_nmr][car_id].keys()
+                    and "license_plate" in results[frame_nmr][car_id].keys()
+                    and "text" in results[frame_nmr][car_id]["license_plate"].keys()
+                ):
+                    f.write(
+                        "{},{},{},{},{},{},{}\n".format(
+                            frame_nmr,
+                            car_id,
+                            "[{} {} {} {}]".format(
+                                results[frame_nmr][car_id]["car"]["bbox"][0],
+                                results[frame_nmr][car_id]["car"]["bbox"][1],
+                                results[frame_nmr][car_id]["car"]["bbox"][2],
+                                results[frame_nmr][car_id]["car"]["bbox"][3],
+                            ),
+                            "[{} {} {} {}]".format(
+                                results[frame_nmr][car_id]["license_plate"]["bbox"][0],
+                                results[frame_nmr][car_id]["license_plate"]["bbox"][1],
+                                results[frame_nmr][car_id]["license_plate"]["bbox"][2],
+                                results[frame_nmr][car_id]["license_plate"]["bbox"][3],
+                            ),
+                            results[frame_nmr][car_id]["license_plate"]["bbox_score"],
+                            results[frame_nmr][car_id]["license_plate"]["text"],
+                            results[frame_nmr][car_id]["license_plate"]["text_score"],
+                        )
+                    )
         f.close()
+
 
 def read_license_plate(license_plate_crop):
     """
@@ -95,20 +103,24 @@ def read_license_plate(license_plate_crop):
     detections = reader.readtext(license_plate_crop)
     print(detections)
 
-    if detections == [] :
+    if detections == []:
         return None, None
 
     for detection in detections:
         bbox, text, score = detection
 
-        #text = text.upper().replace(' ', '')
+        # text = text.upper().replace(' ', '')
         text = text.upper()
         print(text)
 
-        if text is not None and score is not None and bbox is not None and len(text) >= 6:
-        #if license_complies_format(text):
-        #    return format_license(text), score
+        if (
+            text is not None
+            and score is not None
+            and bbox is not None
+            and len(text) >= 6
+        ):
+            # if license_complies_format(text):
+            #    return format_license(text), score
             return text, score
 
     return None, None
-
